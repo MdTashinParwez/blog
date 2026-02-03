4f64d0a1cafcc0dfc18d15d804ff2e95a7126f73
MdTashinParwez <tashinparwez537@gmail.com>
2026-02-03 13:00:12 +0530

    RTE

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,6 +1,6 @@
 const conf = {
   appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
-    appwriteProject: String(import.meta.env.VITE_PROJECT_ID),
+    appwriteProject: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
     appwriteKey: String(import.meta.env.VITE_BUCKET_KEY),
     appwriteDatabase: String(import.meta.env.VITE_DATABASE_ID),
     appwriteCollection: String(import.meta.env.VITE_COLLECTION_ID),
