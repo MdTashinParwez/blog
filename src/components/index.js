d916d562356ade74893aaba3240fc3d79053f295
MdTashinParwez <tashinparwez537@gmail.com>
2026-02-04 18:56:01 +0530

    Pages

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,12 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
-import LogoutBtn from "./Header/LogoutBtn"
-export { Header, Footer, Container, Logo, LogoutBtn };
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import SignUp from "../pages/SignUp";
+import Login from "../pages/Login";
+import Post from "./Post"
+import PostForm from "./post-form/PostForm";
+import Postcard from "./Postcard"
+import AuthLayout from "./AuthLayout"
+export { Header, Footer, Container, Logo, LogoutBtn,RTE,SignUp , Login,Post,PostForm,Postcard,AuthLayout}
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { SignUp as signupComponent } from '../components'
+
+function SignUp() {
+  return (
+    
+      <div className='py-8'>
+        <signupComponent />
+      </div>
+
+    
+  )
+}
+
+export default SignUp
